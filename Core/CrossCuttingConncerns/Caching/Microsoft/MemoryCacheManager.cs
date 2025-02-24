19c83db608edd3d9b81ed979834c24072bbc09b0
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-24 03:36:54 +0300

    Log4Net-Caching-Cross Cutting Concerns

---
+++ Core/CrossCuttingConncerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,63 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConncerns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        private readonly IMemoryCache _memoryCache;
+        private readonly List<string> _cacheKeys;
+        public MemoryCacheManager()
+        {
+            _memoryCache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
+            _cacheKeys = new List<string>();
+        }
+
+
+        public void Add(string key, object value, int duration)
+        {
+            _memoryCache.Set(key, value, TimeSpan.FromMinutes(duration));
+        }
+
+        public T Get<T>(string key)
+        {
+            return _memoryCache.Get<T>(key);
+        }
+
+        public object Get(string key)
+        {
+            return _memoryCache.Get(key);
+        }
+
+        public bool IsAdd(string key)
+        {
+            return _memoryCache.TryGetValue(key, out _);
+
+        }
+
+        public void Remove(string key)
+        {
+            _memoryCache.Remove(key);
+            _cacheKeys.Remove(key);
+        }
+
+        public void RemoveByPattern(string pattern)
+        {
+            var regex = new Regex(pattern, RegexOptions.Singleline | RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            var keysToRemove = _cacheKeys.Where(key => regex.IsMatch(key)).ToList();
+
+            foreach (var key in keysToRemove)
+            {
+                _memoryCache.Remove(key);
+                _cacheKeys.Remove(key);
+            }
+        }
+    }
+}
