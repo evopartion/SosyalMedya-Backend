e408fd1a63c71305115c01d29eab50b73d06cf27
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-27 02:02:51 +0300

    File Helper

---
+++ Web_Presentation/Utilities/Helpers/FileHelper/ExtensionInformation.cs
@@ -0,0 +1,10 @@
+﻿namespace Web_Presentation.Utilities.Helpers.FileHelper
+{
+    public class ExtensionInformation
+    {
+        public string FolderName { get; set; }
+        public string Extension { get; set; }
+        public string MimeType { get; set; }
+        public double MaximumUploadSizeInByte { get; set; }
+    }
+}
---
+++ Web_Presentation/Utilities/Helpers/FileHelper/FileCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Presentation.Utilities.Helpers.FileHelper
+{
+    public class FileCategory
+    {
+        public string FolderName { get; set; }
+        public Dictionary<string, string> ExtensionMimeType { get; set; }
+        public double MaximumUploadSizeInByte { get; set; }
+    }
+}
