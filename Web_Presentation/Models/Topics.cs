7bac56064f955386246bb6aedca300c889a972b4
evopartion <61056779+evopartion@users.noreply.github.com>
2025-02-28 04:28:35 +0300

    Topics Listeleme- Select2 (Partial View)

---
+++ Web_Presentation/Models/Topics.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_Presentation.Models
+{
+    public class Topics
+    {
+        public int Id { get; set; }
+        public string TopicTitle { get; set; }
+    }
+}
---
+++ Web_Presentation/ViewComponents/FormTopic/_FormTopic.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Web_Presentation.Models;
+
+namespace Web_Presentation.ViewComponents.FormTopic
+{
+    public class _FormTopic:ViewComponent
+    {
+            private readonly IHttpClientFactory _httpClientFactory;
+
+            public _FormTopic(IHttpClientFactory httpClientFactory)
+            {
+                _httpClientFactory = httpClientFactory;
+            }
+
+            public async Task<IViewComponentResult> InvokeAsync()
+            {
+                var responseMessage = await _httpClientFactory.CreateClient().GetAsync("https://localhost:44339/api/Topics/getall");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+
+                    var apiDataResponse = JsonConvert.DeserializeObject<ApiListDataResponse<Topics>>(jsonResponse);
+                    //var trueTopic = apiDataResponse.Data.Where(x => x.Status == true).ToList();
+                    return apiDataResponse.Success ? View(apiDataResponse.Data) : View();
+                }
+                return View();
+            }
+        }
+}
